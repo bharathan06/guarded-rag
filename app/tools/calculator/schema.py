7f8dc8bb07ee205080081cbe397af218957018c0
Bharathan <bharathan.m.blr@gmail.com>
2026-08-27 07:20:59 +0530

    middle of building calculator

---
+++ app/tools/calculator/schema.py
@@ -0,0 +1,26 @@
+SCHEMA = {
+    "type": "function",
+    "function":{
+        "name": "calculate",
+        "description": "A tool that can be used to do arithematic calculations",
+        "parameters":{
+            "type": "object",
+            "properties":{
+                "number1": {
+                    "type": "number",
+                    "description": "First number"
+                },
+                "number2" : {
+                    "type" : "number",
+                    "description" : "Second number"
+                },
+                "operator" : {
+                    "type" : "string",
+                    "description" : "The operator used to evaluate the expressions",
+                    "enum" : ["+","-","/","*"]
+                }
+            },
+            "required": ["number1", "number2", "operator"]
+        }
+    }
+}
---
+++ prompts/CHANGELOG.md
@@ -0,0 +1,15 @@
+## [v.v.v] - DD-MM-YY
+### what changed 
+
+### why 
+
+### eval impact
+
+---------------------
+## [0.0.1] - 27-08-26
+### what changed 
+a new system prompt was added 
+### why 
+n/a
+### eval impact
+n/a
